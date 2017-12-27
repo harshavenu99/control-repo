6dc52b7094b5742ab54a387f777dd3b1d581bf18
harshavenu99 <harshavenu99@gmail.com>
2017-12-27 18:40:57 +1100

    Updated node definition in app.pp

--- manifests/app.pp
+++ manifests/app.pp
@@ -1,4 +1,4 @@
-node 'node1' {
+node 'node3' {
     include base
     class {'::tomcat':
         xms            => '60m',
@@ -12,28 +12,4 @@ node 'node1' {
         checksum_value => '1528a0843ed0137678309ce4a7e4f562',
     }
   
-}
-
-node 'node2' {
-    include base
-    class {'::tomcat':
-        xms            => '67m',
-        xmx            => '130m',
-        user           => 'tomcat',
-        group          => 'tomcat',
-        service_status => 'running',
-    }
-    tomcat::deploy { 'sysfoo':
-        deploy_url => 'https://1-115473689-gh.circle-artifacts.com/0/tmp/circle-artifacts.aFBllSS/sysfoo.war',
-        checksum_value => '1528a0843ed0137678309ce4a7e4f562',
-    }
-}
-
-node default {
-    notify {"checkpoint_1":
-        message => "
-            CHECKPINT_1 
-                Applying default code
-        "
-    }
 }
